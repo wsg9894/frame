c489f89d5b797f21ac9f64f1f566f7df1daebe9c
wsg9894 <710731335@qq.com>
2017-09-25 17:24:49 +0800

    yii框架

---
+++ yii2/backend/controllers/IndexController.php
@@ -0,0 +1,11 @@
+<?php
+namespace backend\controllers;
+use yii;
+use yii\web\Controller;
+class IndexController extends Controller
+{
+	public function actionIndex()
+	{
+		echo 'hello world';
+	}
+}
